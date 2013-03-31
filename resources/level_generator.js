c1554faaa440065523a7bc3f79e641b2be47fc50
Jesse Harlin <harlinjesse@gmail.com>
2013-03-31 16:52:14 -0500

    changed randomblock

--- resources/level_generator.js
+++ resources/level_generator.js
@@ -421,7 +421,7 @@ var poop = {};
                 if (rand === 1) {
                     
 
-                    level[x][y] = 'rgba(255, 32, 25, 0.8)';
+                    level[x][y] = 'gba(255, 32, 0, 0.7)';
                 }
 
 
